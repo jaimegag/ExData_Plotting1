e04ee2fc93865edf0f87320176e9d53262c1f3d7
Jaime <jaimegag@gmail.com>
2014-10-08 10:02:21 -0400

    Polishing comments

--- plot1.R
+++ plot1.R
@@ -1,12 +1,13 @@
-# Read data into R
-# Reading all records ... but we could filter 2007-02-01/2 by reading rows 66638 through 69517,
-# both inclusive before accessing the file from R, to save time.
+# plot1.R : Create Histogram on Global_active_power with color Red in plot1.png file
 #
-# Alternatively we could read a txt file previously processed with a Unix script to avoid waisting memory in R
-# and to reduce reading time from 56secs to 0.05 secs (on my computer):
+# Read data into R: Reading all observations
+#
+# Alternatively we could run a Unix script on the dataset to trim out all the rows but the headers
+# and the rows belonging to the 1st and 2nd of February 2007 to avoid waisting memory in R
+# and to reduce reading time from 56secs to 0.05 secs (on my computer). Script :
 #   prompt$> head -1 household_power_consumption.txt > household_power_consumption_1and2_feb_2007.txt
 #   prompt$> grep -E "^[1|2]\/2\/2007" household_power_consumption.txt >> household_power_consumption_1and2_feb_2007.txt
-#   data <- read.table("household_power_consumption_1and2_feb_2007.txt",
+# data <- read.table("household_power_consumption_1and2_feb_2007.txt",
 data <- read.table("household_power_consumption.txt",
                    sep=";",
                    na.strings="?",
